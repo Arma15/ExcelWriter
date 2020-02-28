fc903bbe470686260778bcc296a8542a397f7650
Kevin Florio <kevinf@3dinfotech.com>
2020-02-27 16:37:17 -0800

    updates

--- ExcelWriter/Program.cs
+++ ExcelWriter/Program.cs
@@ -1,38 +1,74 @@
-﻿using OfficeOpenXml;
+﻿using log4net;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
 
 namespace ExcelWriter
 {
     class Program
     {
+        private static readonly ILog _log = LogManager.GetLogger("ExcelWriter.log");
+        private static readonly int[] _doublePositions = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 19, 20, 21, 22 };
+        private static readonly int[] _skipPositions = { 4, 5, 15, 16, 17, 18 };
+        private static readonly int _maxColumn = 22;
+        private static int _startLineOffset = 15000;
+
         static void Main(string[] args)
         {
+            // Test paths
             string excelFilePath = "C:\\Users\\3D Infotech.3DCA-LY520-12\\Desktop\\Example.xlsx";
             string txtFilePath = "C:\\Users\\3D Infotech.3DCA-LY520-12\\Desktop\\Test.txt";
+
             // Command line argument section
             #region Command line
-            /* 
+            
             if (args.Length < 1)
             {
-                Console.WriteLine("Error, no arguments passed.");
-
+               _log.Error("No arguments passed.");
                 return;
             }
-            filePath = args[0];
 
-            Console.WriteLine($"Path passed is: {filePath}");
-
-            if (!File.Exists(filePath))
+            if (args.Length < 2)
             {
-                Console.WriteLine($"File in path: {filePath} does not exist.");
+                _log.Error("Only one argument passed.");
                 return;
             }
-            */
+
+            if (args.Length < 3)
+            {
+                excelFilePath = args[0];
+                txtFilePath = args[1];
+            }
+
+            if (args.Length > 3)
+            {
+                if (Int32.TryParse(args[2], out int num))
+                {
+                    _startLineOffset = num;
+                }
+                else
+                {
+                    _log.Error("Third parameter is not an integer value.");
+                }
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                _log.Error($"File does not exist or path invalid: {args[0]}");
+                return;
+            }
+
+            if (!File.Exists(args[1]))
+            {
+                _log.Error($"File does not exist or path invalid: {args[1]}");
+                return;
+            }
             #endregion Command line
 
 
@@ -41,9 +77,7 @@ namespace ExcelWriter
 
             // Open text file and read all lines
             string[] lines = File.ReadAllLines(txtFilePath);
-            int startLine = 24;
-            
-
+        
             #endregion
 
             //create a fileinfo object of an excel file on the disk (file must exist)
@@ -52,39 +86,63 @@ namespace ExcelWriter
             //create a new Excel package from the file
             using (ExcelPackage excelPackage = new ExcelPackage(file))
             {
+                if (excelPackage.Workbook.Worksheets[1] == null)
+                {
+                    _log.Error("Worksheet does not exist..");
+                    return;
+                }
+
                 //Get a WorkSheet by index. Note that EPPlus indexes are base 1, not base 0!
                 ExcelWorksheet firstWorksheet = excelPackage.Workbook.Worksheets[1];
 
-                /*
-                //Get a WorkSheet by name. If the worksheet doesn't exist, throw an exeption
-                ExcelWorksheet namedWorksheet = excelPackage.Workbook.Worksheets["SomeWorksheet"];
+                // Find first unused row with no date entered
+                while (firstWorksheet.Cells[_startLineOffset, 2].Value != null)
+                {
+                    ++_startLineOffset;
+                }
 
-                //If you don't know if a worksheet exists, you could use LINQ,
-                //So it doesn't throw an exception, but return null in case it doesn't find it
-                ExcelWorksheet anotherWorksheet =
-                    excelPackage.Workbook.Worksheets.FirstOrDefault(x => x.Name == "SomeWorksheet");
-                */
-
-                //Get the content from cells A1 and B1 as string, in two different notations
-                string valA1 = firstWorksheet.Cells["A1"].Value.ToString();
-                string valB1 = firstWorksheet.Cells[1, 2].Value.ToString();
+                _log.Info($"Writing to worksheet, starting at row: {_startLineOffset}");
 
                 // Parse String
-                for (int i = 0; i < lines.Length; ++i)
+                for (int textFileLine = 0; textFileLine < lines.Length; ++textFileLine)
                 {
-                    string[] words = lines[i].Split(',');
-                    for (int j = 0; j < words.Length; ++j)
+                    string[] words = lines[textFileLine].Split(',');
+                    for (int wordsIndex = 0, column = 1; wordsIndex < words.Length && column <= _maxColumn; ++wordsIndex, ++column)
                     {
-                        firstWorksheet.Cells[startLine + i, j+1].Value = words[j];
+                        while (_skipPositions.Contains(column))
+                        {
+                            ++column;
+                        }
+
+                        if (_doublePositions.Contains(column))
+                        {
+                            // If it is a position that is suppose to be a double
+                            if (Double.TryParse(words[wordsIndex], out double number))
+                            {
+                                firstWorksheet.Cells[_startLineOffset + textFileLine, column].Value = number;
+                                continue;
+                            }
+                            else
+                            {
+                                _log.Error($"Index: {wordsIndex} was expected to be a double value, inserted instead as a text value");
+                            }
+                        }
+                        
+                        // Default the rest to text values
+                        firstWorksheet.Cells[_startLineOffset + textFileLine, column].Value = words[wordsIndex];
                         
                     }
                 }
 
-                //add some data
-                //firstWorksheet.Cells[24, 2].Value = "Added data in Cell B4";
-
-                //save the changes
-                excelPackage.Save();
+                try
+                {
+                    //save the changes
+                    excelPackage.Save();
+                }
+                catch (Exception ex)
+                {
+                    _log.Error($"Exception when saving excel document: {ex.Message.ToString()}");
+                }
             }
 
         }
